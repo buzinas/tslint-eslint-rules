6bc8a26353ed6ca80877c8626c989b1d24a66f4f
Vitor Buzinaro <funny.hc@gmail.com>
2015-11-13 14:30:44 -0300

    Fixed #30 - improved build process and enabled mocha debugging

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 'use strict';
 
+const path = require('path');
 const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
 const ts = require('gulp-typescript');
 const mocha = require('gulp-spawn-mocha');
 
 const SRC_FOLDER = 'src/**/*.ts';
+const DEF_FOLDER = 'typings/**/*.ts'
+const project = ts.createProject('tsconfig.json');
 
 gulp.task('build', function build() {
   return gulp
-    .src(SRC_FOLDER)
-    .pipe(ts({
-      'module': 'CommonJS'
+    .src([SRC_FOLDER, DEF_FOLDER, 'node_modules/typescript/lib/lib.es6.d.ts'])
+    .pipe(sourcemaps.init())
+    .pipe(ts(project))
+    .pipe(sourcemaps.write({
+      includeContent: false,
+      sourceRoot: path.join(__dirname, '/src')
     }))
     .pipe(gulp.dest('dist'));
 });
