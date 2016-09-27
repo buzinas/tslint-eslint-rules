93de74b4d33bb4d6c32c047ac995dd57eb92648d
Vitor Buzinaro <funny.hc@gmail.com>
2016-09-27 02:20:28 -0300

    [bug] fixed no-ex-assign false positives (closes #54 and #89)

--- src/rules/noExAssignRule.ts
+++ src/rules/noExAssignRule.ts
@@ -2,7 +2,7 @@ import * as ts from 'typescript';
 import * as Lint from 'tslint/lib/lint';
 
 export class Rule extends Lint.Rules.AbstractRule {
-  public static FAILURE_STRING = 'do not assign to the exception parameter.';
+  public static FAILURE_STRING = 'do not assign to the exception parameter';
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
     const walker = new NoExAssignWalker(sourceFile, this.getOptions());
@@ -12,22 +12,28 @@ export class Rule extends Lint.Rules.AbstractRule {
 
 class NoExAssignWalker extends Lint.RuleWalker {
   private isInCatchClause = false;
-  private currentIdentifier: ts.Identifier = null;
+  private variableNode: ts.VariableDeclaration = null;
 
   protected visitCatchClause(node: ts.CatchClause) {
+    this.variableNode = node.variableDeclaration;
     this.isInCatchClause = true;
     super.visitCatchClause(node);
-    this.currentIdentifier = null;
     this.isInCatchClause = false;
+    this.variableNode = null;
   }
 
-  protected visitIdentifier(node: ts.Identifier) {
-    if (!this.currentIdentifier) {
-      this.currentIdentifier = node;
+  protected visitBinaryExpression(node: ts.BinaryExpression) {
+    if (this.isInCatchClause) {
+      if (node.left.kind === ts.SyntaxKind.Identifier && this.variableNode.name.getText() === node.left.getText()) {
+        this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+      }
+      else if (node.left.kind === ts.SyntaxKind.ArrayLiteralExpression) {
+        const els = (node.left as ts.ArrayLiteralExpression).elements;
+        if (els.some(el => el.getText() === this.variableNode.getText())) {
+          this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+        }
+      }
     }
-    else if (this.currentIdentifier.text === node.text) {
-      this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
-    }
-    super.visitIdentifier(node);
+    super.visitBinaryExpression(node);
   }
 }
--- src/test/rules/noExAssignRuleTests.ts
+++ src/test/rules/noExAssignRuleTests.ts
@@ -8,18 +8,60 @@ const scripts = {
     'try { } catch ({e}) { this.something = 2; }", ecmaFeatures: { destructuring: true } }',
     'function foo() { try { } catch (e) { return false; } }',
     `
-      export function bootstrapComponents(): void {
+    import {Aurelia} from "aurelia-framework";
+    import {bootstrap} from "aurelia-bootstrapper";
+    import {TemplateRegistryEntry, Loader} from "aurelia-loader";
+    import {TextTemplateLoader} from "aurelia-loader-default";
+
+    declare var frontendConfig: { translate: string, debug: boolean };
+
+    (<any>TextTemplateLoader).prototype.standardLoadTemplate = TextTemplateLoader.prototype.loadTemplate;
+    TextTemplateLoader.prototype.loadTemplate = function(loader: Loader, entry: TemplateRegistryEntry): Promise<any> {
+        entry.address += "?translate=" + frontendConfig.language;
+        return (<any>this).standardLoadTemplate(loader, entry);
+    };
+
+    export function bootstrapComponents(): void {
         "use strict";
 
-        bootstrap((aurelia: Aurelia) => {
-          aurelia.use
-            .defaultBindingLanguage()
-            .defaultResources()
-            .developmentLogging();
+        let paths: string[] = [];
+        for (let i = 0; i < arguments.length; i++) {
+            let component: string = arguments[i];
+            paths.push("frontend/components/" + component + "/" + component);
+        }
 
-          aurelia.start().then(()  => aurelia.enhance({}, document.body));
+        bootstrap((aurelia: Aurelia) => { // line 23
+            aurelia.use
+                .defaultBindingLanguage()
+                .defaultResources()
+                .developmentLogging()
+                .globalResources(paths);
+
+            aurelia.start().then(()  => aurelia.enhance({}, document.body));
         });
-      }
+    }
+    `,
+    `
+    import {Component} from '@angular/core';
+
+    @Component({
+
+    })
+    export class App {
+    }
+    `,
+    `
+    describe("The Basic Config", (): void => {
+        it("shall have specific properties attached to it.", (done: (error?: Error) => void): void => {
+            chai.expect(config).to.contain.all.keys(["extends", "rules", "rulesDirectory"]);
+
+            chai.expect(config.extends).to.be.an("array");
+            chai.expect(config.rulesDirectory).to.be.an("array");
+            chai.expect(config.rules).to.be.an("object");
+
+            done();
+        });
+    });
     `
   ],
   invalid: [
@@ -27,7 +69,29 @@ const scripts = {
     'try { } catch (ex) { ex = 10; }',
     'try { } catch (ex) { [ex] = []; }',
     'try { } catch (ex) { ({x: ex = 0}) = {}; }',
-    'try { } catch ({message}) { message = 10; }'
+    `
+    import {Component} from '@angular/core';
+
+    @Component({
+    })
+    export class MyApp {
+      public hi() {
+        try {
+          console.log('Hello World');
+        }
+        catch (e) {
+          // Hi
+          let z = 'xxxx';
+          e = 2;
+          let x = e;
+          z.toString();
+          z = 'gfdg';
+          console.log('Done!', x, z);
+          console.log(e);
+        }
+      }
+    }
+    `
   ]
 };
 
