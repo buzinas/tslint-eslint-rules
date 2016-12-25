0a9a88284f199ec292bcea0534b4323f10a5bc50
Manuel Lopez <jmlopez@iofficecorp.com>
2016-12-24 19:33:41 -0600

    [fix] making sure the identifier is the expression of the property access expression
    We just need to make sure that the identifier is the caller, we can do
    this by making sure that the expression in the PropertyAccessExpression
    is the identifier. If this wasn’t the case, then the expression
    property will be another PropertyAccessExpression.

--- src/rules/handleCallbackErrRule.ts
+++ src/rules/handleCallbackErrRule.ts
@@ -197,8 +197,7 @@ class ErrCallbackHandlerWalker extends Lint.RuleWalker {
         doCheck = !inPropertyAccess;
       } else if (inPropertyAccess) {
         // Make sure we are not in nested property access, i.e. caller.caller.error.prop
-        const isCaller = (node.parent as ts.PropertyAccessExpression).expression === node;
-        doCheck = isCaller && !this.isPropAccess(node.parent.parent);
+        doCheck = (node.parent as ts.PropertyAccessExpression).expression === node;
       } else {
         doCheck = true;
       }
